20529250de3f159cf3abc3ad672486be999f337e
Munir Mardinli <munir.mardinli@icloud.com>
2025-08-21 10:46:54 +0200

    Remove unused MDX files and clean up configuration

--- mdx-components.js
+++ mdx-components.js
@@ -1,9 +1,7 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
  
-// Get the default MDX components
 const themeComponents = getThemeComponents()
  
-// Merge components
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
