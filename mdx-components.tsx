89bc9988ef8d8663828437ffa08df5e59077c9aa
Munir Mardinli <munir.mardinli@icloud.com>
2025-08-27 00:53:35 +0200

    feat: enhance MDX components and add snippets
    - Updated `useMDXComponents` to include a custom image component using Next.js's `Image` with priority loading.
    - Added a reference path for TypeScript in `next-env.d.ts` to improve type checking.
    - Introduced new code snippets for MDX in `.vscode/snippets.code-snippets` to streamline development.
    - Removed unused font files from the public directory to clean up the project.
    - Updated layout component to utilize a new font and improved structure.
    - Deleted the `SidebarAccordion` component as it was no longer needed.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,21 @@
-import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
-import type { MDXComponents } from 'mdx/types'
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs';
+import type { MDXComponents } from 'mdx/types';
+import Image, { ImageProps } from 'next/image';
 
-const themeComponents = getThemeComponents()
+const themeComponents = getThemeComponents();
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...themeComponents,
-    ...components,
-  }
+	return {
+		...themeComponents,
+		...components,
+		img: (props: ImageProps) => (
+			<Image
+				{...props}
+				priority
+				alt={props.alt}
+				width={props.width}
+				height={props.height}
+			/>
+		),
+	};
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,91 +1,91 @@
-import 'dotenv/config'
-import "nextra-theme-docs/style.css";
-import 'katex/dist/katex.min.css'
-import { ThemeProvider } from "@/lib/theme"
-import { Layout, Navbar } from "nextra-theme-docs";
-import { Head, Search } from "nextra/components";
-import { getPageMap } from 'nextra/page-map'
-import { Metadata } from "next";
-import localFont from 'next/font/local';
+import 'dotenv/config';
+import 'nextra-theme-docs/style.css';
+import 'katex/dist/katex.min.css';
+import { ThemeProvider } from '@/lib/theme';
+import { Layout, Navbar } from 'nextra-theme-docs';
+import { Head, Search } from 'nextra/components';
+import { getPageMap } from 'nextra/page-map';
+import { Metadata } from 'next';
 import Database from '@/data/db.json';
 import { GoogleTagManager, GoogleTagManagerNoScript } from '@/lib/googleTag';
-import { SidebarAccordion } from "@/components/SidebarAccordion";
+import { Fragment_Mono } from 'next/font/google';
+import { NextFontWithVariable } from 'next/dist/compiled/@next/font';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_UI_URL!),
-  title: "%s",
-  description: "%s",
+	metadataBase: new URL(process.env.NEXT_PUBLIC_UI_URL!),
+	title: '%s',
+	description: '%s',
 };
 
-export const FontCampany = localFont({
-  src: '../../public/font/Kavivanar-Regular.ttf',
-  display: 'auto',
+const FontCompany: NextFontWithVariable = Fragment_Mono({
+	weight: ['400'],
+	subsets: ['latin'],
+	preload: true,
+	display: 'swap',
+	variable: '--font-fragment-mono',
+	style: ['normal', 'italic'],
+	adjustFontFallback: true,
 });
 
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  const sidebarData = await getPageMap();
-  const baseUrl = process.env.NEXT_PUBLIC_UI_URL!;
+	const sidebarData = await getPageMap();
+	const baseUrl = process.env.NEXT_PUBLIC_UI_URL!;
 
-  return (
-    <html
-      lang="en"
-      className={`${FontCampany.className}`}
-      dir="ltr"
-      suppressHydrationWarning
-    >
-      <Head >
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="sitemap" type="application/xml" href={`${baseUrl}/sitemap.xml`} />
-        <link rel="manifest" href={`${baseUrl}/manifest.webmanifest`} />
-        <meta name="robots" content="index, follow" />
-        <GoogleTagManager />
-      </Head>
-      <body className="antialiased tracking-tight">
-        <GoogleTagManagerNoScript />
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          enableColorScheme
-          storageKey="theme"
-        >
-          <Layout
-            sidebar={{
-              autoCollapse: true,
-              defaultMenuCollapseLevel: 1,
-              defaultOpen: true,
-              toggleButton: true
-            }}
-            nextThemes={{
-              defaultTheme: "system",
-              attribute: "class",
-              storageKey: "theme"
-            }}
-            navbar={
-              <Navbar
-                logoLink="/docs"
-                projectLink={Database.projectLink}
-                logo={<span style={{ marginLeft: ".4em", fontWeight: 800 }}>
-                {Database.headerTitel}
-              </span>}
-                align='left'
-              />
-            }
-            docsRepositoryBase={`${Database.projectLink}/docs`}
-            pageMap={sidebarData}
-            feedback={{ content: null }}
-            search={<Search placeholder="Suche..." />}
-            editLink={false}
-          >
-            <SidebarAccordion />
-            {children}
-          </Layout>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${FontCompany.className}`}
+			dir="ltr"
+			suppressHydrationWarning
+		>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link
+					rel="sitemap"
+					type="application/xml"
+					href={`${baseUrl}/sitemap.xml`}
+				/>
+				<link rel="manifest" href={`${baseUrl}/manifest.webmanifest`} />
+				<meta name="robots" content="index, follow" />
+				<GoogleTagManager />
+			</Head>
+			<body className="antialiased tracking-tight">
+				<GoogleTagManagerNoScript />
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					enableColorScheme
+					storageKey="theme"
+				>
+					<Layout
+						sidebar={{ autoCollapse: true }}
+						navbar={
+							<Navbar
+								logoLink="/docs"
+								projectLink={Database.projectLink}
+								logo={
+									<span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+										{Database.headerTitel}
+									</span>
+								}
+								align="left"
+							/>
+						}
+						docsRepositoryBase={`${Database.projectLink}/docs`}
+						pageMap={sidebarData}
+						feedback={{ content: null }}
+						search={<Search placeholder="Suche..." />}
+						editLink={false}
+					>
+						{children}
+					</Layout>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
